4a4f5473ac61cfe7e885d7916c230c0bc7ecde86
BYONG06 <108468986+BYONG06@users.noreply.github.com>
2023-07-15 03:07:12 +0100

    Update Challenge 3 inner planets.py
    Final code for grid and inner planet orbits. .tracer and .update to skip drawing animation, so remove speed call. Key actually matches.

--- Challenge 3 inner planets.py
+++ Challenge 3 inner planets.py
@@ -6,18 +6,12 @@ mercuryxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 
 venusxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="P,Q").to_numpy()
 earthxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="T,U").to_numpy()
 marsxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="X,Y").to_numpy()
-
-jupiterxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="AB,AC").to_numpy()
-saturnxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="AF,AG").to_numpy()
-uranusxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="AJ,AK").to_numpy()
-neptuneyxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="AN,AO").to_numpy()
-plutoxy = pandas.read_excel("Spreadsheet tasks.xlsx", sheet_name="Challenge 2 (FINAL)", usecols ="AR,AS").to_numpy()
-
 #CALL VALUES IN THESE ARRAYS FROM 1 TO 1001 --> 0 VALUE IS THE COLUMN HEADER
 
 #draw y axis
 axes=turtle.Turtle()
-#axes._tracer(False)
+axes.hideturtle()
+axes._tracer(False)
 for i in range (-200, 250, 50):
     axes.pu()
     axes.goto(i,-200)
@@ -25,28 +19,23 @@ for i in range (-200, 250, 50):
     axes.goto(i, 200)
     axes.pu()
     axes.goto(10, i)
-    axes.pd()
     axes.write(str(i/100))
-#draw x axis
-for i in range (-200, 250, 50):
     axes.pu()
     axes.goto(-200,i)
     axes.pd()
     axes.goto(200, i)
     axes.pu()
     axes.goto(i, -20)
-    axes.pd()
     axes.write(str(i/100))
-axes.hideturtle()
-#axes._update()
+axes._update()
 
 planet=turtle.Turtle()
-#planet._tracer(False)
-planet.speed(10)
+planet._tracer(False)
+planet.hideturtle()
+
 def TYPE(PLANET, color):
     planet.penup()
     planet.color(color)
-
     for i in range(1,1001):
         x = PLANET[i][0]
         y = PLANET[i][1]
@@ -54,8 +43,7 @@ def TYPE(PLANET, color):
         planet.pendown()
 
     planet.goto(PLANET[1][0]*100, PLANET[1][1]*100)
-    planet.hideturtle()
-    #planet._update()
+    planet._update()
 
 TYPE(mercuryxy, "blue")
 TYPE(venusxy, "orange")
@@ -69,7 +57,7 @@ def label(x, y, color, name):
     planet.write(name)
 
 label(250, 210, "black", "Key")
-label(250, 200, "red", "-- Mercury")
+label(250, 200, "blue", "-- Mercury")
 label(250, 190, "orange", "-- Venus")
-label(250, 180, "purple", "-- Earth")
+label(250, 180, "red", "-- Earth")
 label(250, 170, "green", "-- Mars")
