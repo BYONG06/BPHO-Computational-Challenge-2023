bfa2bace65d947bcd45babc34141af912db2e13d
BYONG06 <108468986+BYONG06@users.noreply.github.com>
2023-08-13 16:14:38 +0100

    Update call modules.py
    change to class call instead of function

--- BPHO_master/call modules.py
+++ BPHO_master/call modules.py
@@ -12,25 +12,25 @@ import matplotlib.pyplot as plt
 path = os.path.abspath("Modules")
 sys.path.append(path)
 
-from C1 import Challenge1 #Challenge1()
-from C2 import Challenge2 # Challenge2all()
-from C3_IP import c3IP #c3IP()
-from C3_OP import c3OP() #c3OP()
-from C4_IP import c4IP() #c4IP()
-from C4_OP import c4OP() #c4OP()
-from C5 import Challenge5() #Challenge5()
-from C6 import Challenge6() #Challenge6()
+import C1 #Challenge1()
+import C2 # Challenge2()
+import C3_IP #c3IP()
+import C3_OP #c3OP()
+import C4_IP #c4IP()
+import C4_OP #c4OP()
+import C5  #Challenge5()
+import C6 #Challenge6()
 '''
 RE: Challenge 7
 For 2D versions call IP71(), OP71(), C71()
 For 3D verisons call IP72(), OP72(), C72()
 '''
-from Modules import C7_IP #IP71()
-from Modules import C7_OP #OP71()
-from Modules import C7 #C71()
-from Modules import C7_3D_IP #IP72()
-from Modules import C7_3D_OP #OP72()
-from Modules import C7_3D #C72()
+import C7_IP #IP71()
+import C7_OP #OP71()
+import C7 #C71()
+import C7_3D_IP #IP72()
+import C7_3D_OP #OP72()
+import C7_3D #C72()
 
 
 
